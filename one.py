6241ccfcf95f58e54b2e690f1b3452e0c4023372
leona-1808 <148668181+leona-1808@users.noreply.github.com>
2025-12-31 15:22:49 +0530

    Update one.py

--- one.py
+++ one.py
@@ -1,1 +1,7 @@
-print('Hello world')
+from flask import Flask
+app = Flask(__name__)
+@app.route("/hello", methods = ['GET'])
+def hello():
+  return "Hello Grace"
+if __name__ == "__main__:
+  app.run(debug=True)
