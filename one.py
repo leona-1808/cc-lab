575704c81ee81bb0e7463c97875778b4ef3a28a2
leona-1808 <148668181+leona-1808@users.noreply.github.com>
2026-01-07 16:03:45 +0530

    Update one.py

--- one.py
+++ one.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 @app.route("/hello", methods = ['GET'])
 def hello():
   return "Grace"
-if __name__ == "__main__:
+if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
