4d18ee6ec191e6385f8d7ad9f8d39f41b9757a37
leona-1808 <148668181+leona-1808@users.noreply.github.com>
2026-01-07 16:04:41 +0530

    Update one.py

--- one.py
+++ one.py
@@ -1,7 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
-@app.route("/hello", methods = ['GET'])
+
+@app.route("/")
+def home():
+    return "Flask App Running Successfully"
+
+@app.route("/hello", methods=["GET"])
 def hello():
-  return "Grace"
+    return "Grace"
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)
